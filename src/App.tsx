fbc3064806399f49af3d4de9503a045b700f4b2b
rafimps18 <impasrafael@gmail.com>
2023-06-10 20:57:50 +0800

    Made some major changes with the header design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/navbar/Navbar"
+import Header from "./components/header/Header"
 
 function App() {
   return (
-    <Navbar />
-  )
+    <Header />
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,23 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <>
+            <header className="header">
+                <div className="logo">
+                    <img src="src/components/assets/logo.png" alt="logo" />
+                    <a className="name" href="#">Rafael Impas</a>
+                </div>
+                <nav className="nav-container">
+                    <ul>
+                        <li><a href="#">My Projects</a></li>
+                        <li><a href="#">Contact Me</a></li>
+                        <li><a href="https://github.com/rafimps18">My Github</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header;
