6fe70b3440ce3300a3df544a85ebc7b424f5d859
rafimps18 <impasrafael@gmail.com>
2023-06-11 21:41:01 +0800

    Made some small changes

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -8,21 +8,21 @@ const Projects = () => {
             <div className="projects-header">
                 <h1>My Projects</h1>
             </div>
-            <div className="projects-catalog">
-                <ProjectCard 
-                imgsrc=".\src\components\assets\SW-PIMS.png"
-                projectName="Product Inventory Management System for a Shoe store" 
-                projectDetails="A product inventory management that has basic CRUD functions and a Database"
+            <div className="projects-catalog" id="projects-catalog">
+                <ProjectCard
+                    imgsrc=".\src\components\assets\SW-PIMS.png"
+                    projectName="Product Inventory Management System for a Shoe store"
+                    projectDetails="A product inventory management that has basic CRUD functions and a Database"
                 />
-                <ProjectCard 
-                imgsrc=".\src\components\assets\SW-PIMS.png"
-                projectName="Product Inventory Management System for a Shoe store" 
-                projectDetails="A product inventory management that has basic CRUD functions and a Database"
+                <ProjectCard
+                    imgsrc=".\src\components\assets\SW-PIMS.png"
+                    projectName="Product Inventory Management System for a Shoe store"
+                    projectDetails="A product inventory management that has basic CRUD functions and a Database"
                 />
-                <ProjectCard 
-                imgsrc=".\src\components\assets\SW-PIMS.png"
-                projectName="Product Inventory Management System for a Shoe store" 
-                projectDetails="A product inventory management that has basic CRUD functions and a Database"
+                <ProjectCard
+                    imgsrc=".\src\components\assets\SW-PIMS.png"
+                    projectName="Product Inventory Management System for a Shoe store"
+                    projectDetails="A product inventory management that has basic CRUD functions and a Database"
                 />
             </div>
         </div>
