73f2ca5384e90af307b4239a78b4636f21552241
rafimps18 <impasrafael@gmail.com>
2023-06-15 07:37:00 +0800

    Added another project to projects section

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,4 +1,5 @@
-import projectSnapshot from "/src/assets/SW-PIMS.png";
+import projectPIMS from "/src/assets/SW-PIMS.png";
+import projectTodo from "/src/assets/todo.png";
 import ProjectCard from '../project-card/ProjectCard';
 import './Projects.css';
 
@@ -11,9 +12,15 @@ const Projects = () => {
             <div className="projects-catalog" id="projects-catalog">
 
                 <ProjectCard
-                    imgsrc={projectSnapshot}
+                    imgsrc={projectPIMS}
                     projectName="Product Inventory Management System for a Shoe store"
                     projectDetails="A product inventory management that has basic CRUD functions and a Database"
+                />
+
+                <ProjectCard
+                    imgsrc={projectTodo}
+                    projectName="To do app"
+                    projectDetails="A simple to do app that persists data with the help of local storage. Still a Work in progress."
                 />
             </div>
         </div>
