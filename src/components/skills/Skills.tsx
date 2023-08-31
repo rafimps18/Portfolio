a6efb8b97d2bb4a031d89601ebe2ad0a74dbb3a4
rafimps18 <impasrafael@gmail.com>
2023-08-31 18:34:08 +0800

    Added icons for languages

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -9,44 +9,41 @@ const MySkills = () => {
                         <h1>My Skills</h1>
                         <h2>Languages</h2>
                         <div className="language-list">
-                            <div className="language-list-item">
-                                <h3>HTML</h3>
-                            </div>
-                            <div className="language-list-item">
-                                <h3>CSS</h3>
-                            </div>
-                            <div className="language-list-item">
-                                <h3>JavaScript</h3>
-                            </div>
-                            <div className="language-list-item">
-                                <h3>PHP</h3>
-                            </div>
-                            <div className="language-list-item">
-                                <h3>Python</h3>
-                            </div>
-                            <div className="language-list-item">
-                                <h3>Java</h3>
-                            </div>
+                            <div className="language-list-row">
+                                <div className="language-list-item">
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" />
+                                    <h3>HTML</h3>
+                                </div>
+                                <div className="language-list-item">
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" />
+                                    <h3>CSS</h3>
+                                </div>
+                                <div className="language-list-item">
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+                                    <h3>JavaScript</h3>
+                                </div>
+                                <div className="language-list-item">
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg" />
+
+                                    <h3>PHP</h3>
+                                </div>
+                                <div className="language-list-item">
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+
+                                    <h3>Python</h3>
+                                </div></div>
                         </div>
-                        <h2>Tools</h2>
+                        <h2>Frameworks and Libraries</h2>
                         <div className="language-list">
-                            <div className="language-list-item">
-                                <h3>Postman</h3>
-                            </div>
-                            <div className="language-list-item">
-                                <h3>Xampp</h3>
-                            </div>
-                            <div className="language-list-item">
-                                <h3>React</h3>
-                            </div>
-                            <div className="language-list-item">
-                                <h3>Chrome Developer Console</h3>
-                            </div>
-                            <div className="language-list-item">
-                                <h3>VS Code</h3>
-                            </div>
-                            <div className="language-list-item">
-                                <h3>Canva</h3>
+                            <div className="language-list-row">
+                                <div className="language-list-item">
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+                                    <h3>React</h3>
+                                </div>
+                                <div className="language-list-item">
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain-wordmark.svg" />
+                                    <h3>Laravel</h3>
+                                </div>
                             </div>
                         </div>
                     </div>
